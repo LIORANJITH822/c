c855cd2c9d0396409a939017fc83f1142a29e9c2
LIORANJITH822 <35991661+LIORANJITH822@users.noreply.github.com>
2018-02-07 14:42:53 +0530

    Create ranjith37.c

---
+++ ranjith37.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    void main()
+    {
+                  int x, y;
+                  printf("Enter any two integer numbers : ");
+                  scanf("%d %d", &x, &y);
+                  printf("\n The value of x before swapping is %d", x);
+                  printf("\n The value of y before swapping is %d", y);
+                  x = x + y;
+                  y = x - y;
+                  x = x - y;
+                  printf("\n The value of x after swapping is %d", x);
+                  printf("\n The value of y after swapping is %d", y);
+    getch();
+    }
