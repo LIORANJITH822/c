90d53563683ee1ace2d2764ab0a6d33b64e6f53b
LIORANJITH822 <35991661+LIORANJITH822@users.noreply.github.com>
2018-02-07 14:15:49 +0530

    Create ranjith31.c

---
+++ ranjith31.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+char str[50];
+int words = 0, len = 0, i;
+clrscr();
+printf(“\n\n\t ENTER A STRING…: “);
+gets(str);
+while(str[len]!=’\0′)
+len++;
+len–;
+for(i=0;i<=len;i++)
+{
+if ( ( str[i] == ‘ ‘ && str[i+1] != ‘ ‘ ) || i == len )
+words++;
+}
+printf(“\n\t NUMBER OF WORDS IN THE ABOVE SENTENCE IS…: %d”, words);
+getch();
+}
